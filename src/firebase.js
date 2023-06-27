c9bb8d3912f9556e3e792e67c99a5de9e8ad03a9
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-27 12:12:55 +0530

    Environment Error Resolved

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    databaseURL: process.env.databaseURL,
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    databaseURL: process.env.REACT_APP_databaseURL,
 };
 
 export const app = initializeApp(firebaseConfig);  
