9aadbc45e5150de8de243cc7eaf614c84be73369
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-27 11:36:42 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2tHt6HI3yEN8i02pKcLnXJhEPLj-vops",
+    authDomain: "app-d0be5.firebaseapp.com",
+    projectId: "app-d0be5",
+    storageBucket: "app-d0be5.appspot.com",
+    messagingSenderId: "428548745231",
+    appId: "1:428548745231:web:b4e609f0a8375ca0cb9b7a",
+    databaseURL: "https://app-d0be5-default-rtdb.firebaseio.com/",
+};
+
+export const app = initializeApp(firebaseConfig);  
